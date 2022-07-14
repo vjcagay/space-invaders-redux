d408bc4304e40a675ca9568d1888b1a7da1e336e
Vincent John Cagay <iam@vjca.gay>
2022-07-14 16:06:58 +0900

    Refactor game logic

--- public/js/index.js
+++ public/js/index.js
@@ -6,39 +6,38 @@ import { Sprite } from "./components/sprite.js";
 let canvasWidth = 0;
 let canvasHeight = 0;
 
-let lastUpdateTimestamp = 0;
 let animationFrameId = 0;
 
-const MAX_ENEMIES = 10;
-const ENEMY_MOVE_PIXEL = 10;
-const ENEMY_SIZE_MULTIPLIER = 10;
+const ALIEN_MAX_POPULATION = 10;
+const ALIEN_MOVE_PIXEL = 10;
+const ALIEN_SIZE_MULTIPLIER = 10;
 const MISSILE_MOVE_PIXEL = -10;
 
 let alienLastUpdateTimeStamp = 0;
 let missileLastUpdateTimeStamp = 0;
 
+let alienPopulation = 0;
+let missilePopulation = 0;
+
 let turret;
 
 const run = () => {
-  // Objects move downwards
-
   const now = Date.now();
 
+  // Spawn and move aliens
   if (now - alienLastUpdateTimeStamp >= 1000) {
-    if (Sprite.instances.size > 1) {
-      // Move all enemies
-      for (const [_, instance] of Sprite.instances.entries()) {
-        if (instance.name === "alien") {
-          instance.move(0, ENEMY_MOVE_PIXEL);
-        }
-      }
+    if (alienPopulation < ALIEN_MAX_POPULATION) {
+      spawnAlien();
+    }
 
-      // Check for collisions. If an enemy passes threshold, game over.
-      for (const [_, instance] of Sprite.instances.entries()) {
-        if (
-          instance.name === "alien" &&
-          instance.coords.y + instance.size.height >= canvasHeight
-        ) {
+    for (const [_, instance] of Sprite.instances.entries()) {
+      if (instance.name === "alien") {
+        // Aliens move downwards
+        instance.move(0, ALIEN_MOVE_PIXEL);
+
+        // Check if alien reaches the bottom of the canvas.
+        // And if it is, game over.
+        if (instance.coords.y + instance.size.height >= canvasHeight) {
           instance.explode();
           turret.explode();
           alert("Game Over!");
@@ -48,86 +47,6 @@ const run = () => {
       }
     }
 
-    // Generate an enemy per tick until max number
-    if (Alien.instances.size < MAX_ENEMIES) {
-      const spawnEnemy = () => {
-        // Min 10px, Max 100px
-        const size =
-          (Math.floor(Math.random() * 10) + 1) * ENEMY_SIZE_MULTIPLIER;
-        let xAxis = Math.floor(Math.random() * canvasWidth + 1) - size;
-
-        // Enemy should not hang off the left edge of the canvas
-        if (xAxis < 0) {
-          xAxis = 0;
-        }
-
-        // Enemy should not hang off the right edge of the canvas
-        if (xAxis + size > canvasWidth) {
-          xAxis = canvasWidth - size;
-        }
-
-        let canSpawn = true;
-
-        // Check for existing enemies for possible collisions when the new enemy spawns
-        // We want to avoid that, otherwise enemies spawning can stack on top of each other and it does not look nice.
-        for (const [_, instance] of Alien.instances.entries()) {
-          if (canSpawn) {
-            const instanceLeftSideXAxis = instance.coords.x;
-            const instanceRightSideXAxis =
-              instanceLeftSideXAxis + instance.size.width;
-            const spawnLeftSideXAxis = xAxis;
-            const spawnRightSideXAxis = xAxis + size;
-
-            const instanceTopSideYAxis = instance.coords.y;
-            const instanceBottomSideYAxis =
-              instanceTopSideYAxis + instance.size.height;
-            // Enemy spawns always at 0 so no need to check for the top side
-            // So the bottom value will always correspond to the spawn's size
-            const spawnBottomSideYAxis = size;
-
-            if (spawnBottomSideYAxis < instanceBottomSideYAxis) {
-              // These conditions will guarantee a collision, so prevent spawning
-
-              // Enemy spawn's left side rests on existing enemy's body
-              if (
-                spawnLeftSideXAxis >= instanceLeftSideXAxis &&
-                spawnLeftSideXAxis <= instanceRightSideXAxis
-              ) {
-                canSpawn = false;
-              }
-
-              // Enemy spawn's right side rests on existing enemy's body
-              if (
-                spawnRightSideXAxis >= instanceLeftSideXAxis &&
-                spawnRightSideXAxis <= instanceRightSideXAxis
-              ) {
-                canSpawn = false;
-              }
-
-              // Enemy spawn is bigger and its body covers existing enemy
-              if (
-                spawnLeftSideXAxis <= instanceLeftSideXAxis &&
-                spawnRightSideXAxis >= instanceRightSideXAxis
-              ) {
-                canSpawn = false;
-              }
-            } else {
-              canSpawn = false;
-            }
-          }
-        }
-
-        // If the spawning can't be done this time, then just try again on the next
-        if (canSpawn) {
-          new Alien(canvas, size)
-            .render(xAxis)
-            .onPress(moveTurretAndFireToTarget);
-        }
-      };
-
-      spawnEnemy();
-    }
-
     alienLastUpdateTimeStamp = now;
   }
 
@@ -135,9 +54,13 @@ const run = () => {
   if (now - missileLastUpdateTimeStamp >= 50) {
     for (const [_, instance] of Sprite.instances.entries()) {
       if (instance.name === "missile") {
+        // Missiles move upwards
         instance.move(0, MISSILE_MOVE_PIXEL);
+
+        // If missile goes out of bounds, destroy it
         if (instance.coords.y < 0) {
           instance.destroy();
+          missilePopulation--;
         }
       }
     }
@@ -146,6 +69,80 @@ const run = () => {
   }
 
   animationFrameId = window.requestAnimationFrame(run);
+};
+
+const spawnAlien = () => {
+  // Min 10px, Max 100px
+  const size =
+    (Math.floor(Math.random() * ALIEN_SIZE_MULTIPLIER) + 1) *
+    ALIEN_SIZE_MULTIPLIER;
+  let xAxis = Math.floor(Math.random() * canvasWidth + 1) - size;
+
+  // Enemy should not hang off the left edge of the canvas
+  if (xAxis < 0) {
+    xAxis = 0;
+  }
+
+  // Enemy should not hang off the right edge of the canvas
+  if (xAxis + size > canvasWidth) {
+    xAxis = canvasWidth - size;
+  }
+
+  let canSpawn = true;
+
+  // Check for existing enemies for possible collisions when the new enemy spawns
+  // We want to avoid that, otherwise enemies spawning can stack on top of each other and it does not look nice.
+  for (const [_, instance] of Alien.instances.entries()) {
+    if (canSpawn) {
+      const instanceLeftSideXAxis = instance.coords.x;
+      const instanceRightSideXAxis =
+        instanceLeftSideXAxis + instance.size.width;
+      const spawnLeftSideXAxis = xAxis;
+      const spawnRightSideXAxis = xAxis + size;
+
+      const instanceTopSideYAxis = instance.coords.y;
+      const instanceBottomSideYAxis =
+        instanceTopSideYAxis + instance.size.height;
+      // Enemy spawns always at 0 so no need to check for the top side
+      // So the bottom value will always correspond to the spawn's size
+      const spawnBottomSideYAxis = size;
+
+      if (spawnBottomSideYAxis < instanceBottomSideYAxis) {
+        // These conditions will guarantee a collision, so prevent spawning
+
+        // Enemy spawn's left side rests on existing enemy's body
+        if (
+          spawnLeftSideXAxis >= instanceLeftSideXAxis &&
+          spawnLeftSideXAxis <= instanceRightSideXAxis
+        ) {
+          canSpawn = false;
+        }
+
+        // Enemy spawn's right side rests on existing enemy's body
+        if (
+          spawnRightSideXAxis >= instanceLeftSideXAxis &&
+          spawnRightSideXAxis <= instanceRightSideXAxis
+        ) {
+          canSpawn = false;
+        }
+
+        // Enemy spawn is bigger and its body covers existing enemy
+        if (
+          spawnLeftSideXAxis <= instanceLeftSideXAxis &&
+          spawnRightSideXAxis >= instanceRightSideXAxis
+        ) {
+          canSpawn = false;
+        }
+      } else {
+        canSpawn = false;
+      }
+    }
+  }
+
+  // If the spawning can't be done this time, then just try again on the next
+  if (canSpawn) {
+    new Alien(canvas, size).render(xAxis).onPress(moveTurretAndFireToTarget);
+  }
 };
 
 const moveTurretAndFireToTarget = (target) => {
@@ -165,6 +162,7 @@ const moveTurretAndFireToTarget = (target) => {
    */
   const xAxisValueToMoveInto = targetWidth / 2 - turretWidth / 2 + targetXAxis;
 
+  // Turret move sideways
   turret.move(xAxisValueToMoveInto - turretXAxis, 0);
 
   // Let's load the missile
@@ -179,6 +177,8 @@ const moveTurretAndFireToTarget = (target) => {
 
   // Fire away
   missile.render(missileXAxis, missileYAxis);
+
+  missilePopulation++;
 };
 
 window.addEventListener("DOMContentLoaded", () => {
