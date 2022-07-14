655614b793c9638b5192b45b5bff3485a4a7301a
Vincent John Cagay <iam@vjca.gay>
2022-07-14 21:40:54 +0900

    Implement game Start/Pause/Continue functions

--- public/js/index.js
+++ public/js/index.js
@@ -18,107 +18,141 @@ let missilePopulation = 0;
 let turret;
 
 let isGameOn = false;
+let isGamePaused = false;
 
+// Do what need to be done each update
 const run = () => {
-  if (isGameOn) {
-    const now = Date.now();
+  if (!isGamePaused) {
+    if (isGameOn) {
+      const now = Date.now();
 
-    // Spawn and move aliens
-    if (now - alienLastUpdateTimeStamp >= 1000) {
-      // Try spawning until the max population is reached
-      if (alienPopulation < ALIEN_MAX_POPULATION) {
-        const alien = spawnAlien(canvas);
-        if (alien) {
-          alien.onPress((thisAlien) => {
-            moveTurretToTarget(turret, thisAlien);
-            fireMissile(canvas, turret);
-            missilePopulation++;
-          });
-          alienPopulation++;
-        }
-      }
-
-      // For each alien;
-      // 1. Check any dead aliens and remove them from the canvas
-      // 2. Living aliens will keep on moving until they reach the bottom of the canvas
-      for (const spriteInstance of Sprite.instances.values()) {
-        if (spriteInstance.name === "alien") {
-          const alien = spriteInstance;
-
-          if (alien.dead) {
-            alien.destroy();
-          } else {
-            // Aliens move downwards
-            alien.move(0, ALIEN_MOVE_PIXEL);
-          }
-
-          // Keep the game running until an alien reaches the bottom of the canvas.
-          if (alien.coords.y + alien.size.height >= store.canvasHeight) {
-            alien.explode();
-            turret.explode();
-
-            // Game over!
-            isGameOn = false;
-
-            // Stop the loop
-            break;
+      // Spawn and move aliens
+      if (now - alienLastUpdateTimeStamp >= 1000) {
+        // Try spawning until the max population is reached
+        if (alienPopulation < ALIEN_MAX_POPULATION) {
+          const alien = spawnAlien(canvas);
+          if (alien) {
+            alien.onPress((thisAlien) => {
+              moveTurretToTarget(turret, thisAlien);
+              fireMissile(canvas, turret);
+              missilePopulation++;
+            });
+            alienPopulation++;
           }
         }
+
+        // For each alien;
+        // 1. Check any dead aliens and remove them from the canvas
+        // 2. Living aliens will keep on moving until they reach the bottom of the canvas
+        for (const spriteInstance of Sprite.instances.values()) {
+          if (spriteInstance.name === "alien") {
+            const alien = spriteInstance;
+
+            if (alien.dead) {
+              alien.destroy();
+            } else {
+              // Aliens move downwards
+              alien.move(0, ALIEN_MOVE_PIXEL);
+            }
+
+            // Keep the game running until an alien reaches the bottom of the canvas.
+            if (alien.coords.y + alien.size.height >= store.canvasHeight) {
+              alien.explode();
+              turret.explode();
+
+              // Game over!
+              isGameOn = false;
+
+              // Stop the loop
+              break;
+            }
+          }
+        }
+
+        alienLastUpdateTimeStamp = now;
       }
 
-      alienLastUpdateTimeStamp = now;
-    }
+      // Move the missiles
+      if (now - missileLastUpdateTimeStamp >= 50) {
+        for (const spriteInstance of Sprite.instances.values()) {
+          if (spriteInstance.name === "missile") {
+            const missile = spriteInstance;
 
-    // Move the missiles
-    if (now - missileLastUpdateTimeStamp >= 50) {
-      for (const spriteInstance of Sprite.instances.values()) {
-        if (spriteInstance.name === "missile") {
-          const missile = spriteInstance;
+            // Missiles move upwards
+            missile.move(0, MISSILE_MOVE_PIXEL);
 
-          // Missiles move upwards
-          missile.move(0, MISSILE_MOVE_PIXEL);
+            // If missile goes out of the canvas, destroy it
+            if (missile.coords.y < 0) {
+              missile.destroy();
+              missilePopulation--;
+            } else {
+              // Check if an alien hits the missile
+              for (const anotherSpriteInstance of Sprite.instances.values()) {
+                if (anotherSpriteInstance.name === "alien") {
+                  const alien = anotherSpriteInstance;
 
-          // If missile goes out of the canvas, destroy it
-          if (missile.coords.y < 0) {
-            missile.destroy();
-            missilePopulation--;
-          } else {
-            // Check if an alien hits the missile
-            for (const anotherSpriteInstance of Sprite.instances.values()) {
-              if (anotherSpriteInstance.name === "alien") {
-                const alien = anotherSpriteInstance;
+                  const alienBottomSide = alien.coords.y + alien.size.height;
+                  const alienLeftSide = alien.coords.x;
+                  const alienRightSide = alienLeftSide + alien.size.width;
 
-                const alienBottomSide = alien.coords.y + alien.size.height;
-                const alienLeftSide = alien.coords.x;
-                const alienRightSide = alienLeftSide + alien.size.width;
+                  // If an alien hits the missile
+                  // 1. Make the alien explode. It will be destroyed on the next frame update
+                  // 2. Destroy the missile.
+                  if (
+                    missile.coords.y <= alienBottomSide &&
+                    missile.coords.x >= alienLeftSide &&
+                    missile.coords.x <= alienRightSide
+                  ) {
+                    alien.explode();
+                    missile.destroy();
 
-                // If an alien hits the missile
-                // 1. Make the alien explode. It will be destroyed on the next frame update
-                // 2. Destroy the missile.
-                if (
-                  missile.coords.y <= alienBottomSide &&
-                  missile.coords.x >= alienLeftSide &&
-                  missile.coords.x <= alienRightSide
-                ) {
-                  alien.explode();
-                  missile.destroy();
-
-                  alienPopulation--;
-                  missilePopulation--;
+                    alienPopulation--;
+                    missilePopulation--;
+                  }
                 }
               }
             }
           }
         }
+
+        missileLastUpdateTimeStamp = now;
       }
 
-      missileLastUpdateTimeStamp = now;
+      window.requestAnimationFrame(run);
+    } else {
+      alert("Game Over!");
+      cleanup();
     }
-
-    window.requestAnimationFrame(run);
-  } else {
-    alert("Game Over!");
   }
+};
+
+// Start the game
+const start = () => {
+  turret = new Turret(canvas);
+
+  // Render the turret in the bottom center
+  turret.render(
+    store.canvasWidth / 2 - turret.size.width / 2,
+    store.canvasHeight - turret.size.height
+  );
+
+  missileLastUpdateTimeStamp = Date.now();
+  alienLastUpdateTimeStamp = Date.now();
+
+  isGameOn = true;
+
+  run();
+};
+
+// Remove all the sprites in the canvas
+const cleanup = () => {
+  for (const spriteInstance of Sprite.instances.values()) {
+    spriteInstance.destroy();
+  }
+
+  button.textContent = "Start";
+  isGameOn = false;
+  isGamePaused = false;
 };
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -133,23 +167,26 @@ window.addEventListener("DOMContentLoaded", () => {
     window.getComputedStyle(canvas).height.replace("px", "")
   );
 
+  // Put these values in the store to be used on other parts of the game
   store.canvasWidth = canvasWidth;
   store.canvasHeight = canvasHeight;
 
+  // Button that switches its text to "Start"/"Pause"/"Continue"
   button.addEventListener("click", () => {
-    turret = new Turret(canvas);
+    if (isGameOn) {
+      if (isGamePaused) {
+        isGamePaused = false;
+        run();
+      } else {
+        isGamePaused = true;
+      }
+    } else {
+      isGameOn = true;
+      start();
+    }
 
-    // Render the turret in the bottom center
-    turret.render(
-      canvasWidth / 2 - turret.size.width / 2,
-      canvasHeight - turret.size.height
-    );
-
-    missileLastUpdateTimeStamp = Date.now();
-    alienLastUpdateTimeStamp = Date.now();
-
-    isGameOn = true;
-
-    run();
+    if (isGameOn) {
+      button.textContent = isGamePaused ? "Continue" : "Pause";
+    }
   });
 });
